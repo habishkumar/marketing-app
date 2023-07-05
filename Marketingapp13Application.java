6572cd17fbdaf06ca891e7f997318f6e016a65de
habishkumar <136294276+habishkumar@users.noreply.github.com>
2023-07-05 18:20:32 +0530

    Add files via upload

---
+++ Marketingapp13Application.java
@@ -0,0 +1,13 @@
+package com.marketingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Marketingapp13Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Marketingapp13Application.class, args);
+	}
+
+}
